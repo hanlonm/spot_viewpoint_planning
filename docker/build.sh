ac4371fb18815d95ceedab58e7892110778b44fc
Matthew Hanlon <hanlonm@ethz.ch>
2023-08-03 16:36:50 +0200

    build.sh

---
+++ docker/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+
+ROS_DISTRO="noetic"
+BASE_IMAGE="osrf/ros:$ROS_DISTRO-desktop"
+IMAGE_NAME="spot_viewpoint_planning:0.0.1"
+
+while getopts r:b:t: flag
+do
+    case "${flag}" in
+        r) ROS_DISTRO=${OPTARG};;
+        b) BASE_IMAGE=${OPTARG};;
+        t) IMAGE_NAME=${OPTARG};;
+        *) error "Unexpected option ${flag}" ;;
+    esac
+done
+
+docker build \
+    -t $IMAGE_NAME \
+    --build-arg ROS_DISTRO=$ROS_DISTRO \
+    --build-arg BASE_IMAGE=$BASE_IMAGE \
+    -f Dockerfile ${SCRIPT_DIR}/..
