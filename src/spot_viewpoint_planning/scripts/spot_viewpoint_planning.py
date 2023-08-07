c7f6df3e3e13214515d9627a1b3282721b3c05e5
BoySun045 <boyang-sun@outlook.com>
2023-08-07 17:15:32 +0200

    testing on robot

--- src/spot_viewpoint_planning/scripts/spot_viewpoint_planning.py
+++ src/spot_viewpoint_planning/scripts/spot_viewpoint_planning.py
@@ -16,7 +16,7 @@ class SpotViewpointPlanner:
     def __init__(self) -> None:
         
         self.test_service = rospy.Service("query_viewpoint", Empty, self.query_viewpoint_cb)
-        self.test_service = rospy.Service("next_waypoint", Empty, self.query_viewpoint_cb)
+        self.next_wp_service = rospy.Service("next_waypoint", Empty, self.next_waypoint_cb)
         self.home_arm_service = rospy.Service("home_arm", Empty, self.home_arm_cb)
         self.gripper_pose_proxy = rospy.ServiceProxy("/spot/gripper_pose", HandPose)
 
@@ -48,11 +48,83 @@ class SpotViewpointPlanner:
                           [-1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
                           [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0],
                           ]
-        self.map_poses = [[1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
-                          [0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0],
-                          [-1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
-                          [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+        self.map_poses = [[11.84244155883789, -2.155806064605713, 0.0, 0.7114715562211398, 0.0, 0.0, 0.7027148957352971],
+                          [11.389702796936035, -1.5879647731781006, 0.0, 0.005799954052785274, 0.0, 0.0, -0.9999831801250387],
+                          [11.224780082702637, -4.262598514556885, 0.0, 0.7292919090780428, 0.0, 0.0, -0.6842026829480455],
+                          [12.032266616821289, -4.588181495666504, 0.0, 0.9983694875169599, 0.0, 0.0, 0.05708210223111083],
                           ]
+        
+        """
+
+        header:
+        stamp:
+            sec: 1691401559
+            nanosec: 741685544
+        frame_id: world
+        pose:
+        position:
+            x: 11.84244155883789
+            y: -2.155806064605713
+            z: 0.0
+        orientation:
+            x: 0.0
+            y: 0.0
+            z: 0.7027148957352971
+            w: 0.7114715562211398
+        ---
+        header:
+        stamp:
+            sec: 1691401569
+            nanosec: 164142233
+        frame_id: world
+        pose:
+        position:
+            x: 11.389702796936035
+            y: -1.5879647731781006
+            z: 0.0
+        orientation:
+            x: 0.0
+            y: 0.0
+            z: -0.9999831801250387
+            w: 0.005799954052785274
+        ---
+        header:
+        stamp:
+            sec: 1691401574
+            nanosec: 499579242
+        frame_id: world
+        pose:
+        position:
+            x: 11.224780082702637
+            y: -4.262598514556885
+            z: 0.0
+        orientation:
+            x: 0.0
+            y: 0.0
+            z: -0.6842026829480455
+            w: 0.7292919090780428
+        ---
+        header:
+        stamp:
+            sec: 1691401580
+            nanosec: 995543056
+        frame_id: world
+        pose:
+        position:
+            x: 12.032266616821289
+            y: -4.588181495666504
+            z: 0.0
+        orientation:
+            x: 0.0
+            y: 0.0
+            z: 0.05708210223111083
+            w: 0.9983694875169599
+        ---
+        
+        
+        
+        """
+
         self.pose_it = 0
 
         self.tf_buffer = tf2_ros.Buffer()
@@ -61,14 +133,17 @@ class SpotViewpointPlanner:
         self.plan_viewpoint_pub = rospy.Publisher("plan_viewpoint", TransformStamped)
         self.plan_viewpoint_result_sub = rospy.Subscriber("plan_viewpoint_result_loc_cam", PoseStamped, self.viewpoint_result_cb)
 
+        self.goal_pose_sub = rospy.Subscriber("/goal_pose", PoseStamped, self.goal_pose_cb)
+
     def viewpoint_result_cb(self, msg: PoseStamped):
         quat = np.array([msg.pose.orientation.w,msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z])
         euler_zyx = pr.euler_from_quaternion(quat, 2,1,0, False)
-        arm_angles = self.base_arm_angles
-        arm_angles[0] = arm_angles + euler_zyx[0]
-        arm_angles[4] = arm_angles + euler_zyx[1]
+        arm_angles = self.base_arm_angles.copy()
+        arm_angles[0] = arm_angles[0] + euler_zyx[0]
+        arm_angles[4] = arm_angles[4] + euler_zyx[1]
         arm_angles_req = ArmJointMovementRequest()
         arm_angles_req.joint_target = arm_angles
+        print(arm_angles)
         self.arm_angle_proxy.call(arm_angles_req)
 
     def query_viewpoint_cb(self, req: EmptyRequest):
@@ -78,18 +153,29 @@ class SpotViewpointPlanner:
     
     def home_arm_cb(self, req: EmptyRequest):
         self.arm_angle_proxy.call(self.base_arm_angles_request)
+
+        return EmptyResponse()
     
     def next_waypoint_cb(self, req: EmptyRequest):
         self.go_to_map_pose()
+
+        return EmptyResponse()
     
     def go_to_map_pose(self):
-        if self.pose_it == len(self.map_poses)-1:
+        if self.pose_it == len(self.map_poses):
             self.pose_it = 0
         pose_msg = self.pq_to_pose_msg(self.map_poses[self.pose_it])
         go_to_pose = PoseStamped()
         go_to_pose.header.frame_id = "map"
         go_to_pose.pose = pose_msg
-        self.move_spot_pub(go_to_pose)
+        self.move_spot_pub.publish(go_to_pose)
+        self.arm_angle_proxy.call(self.base_arm_angles_request)
+
+        self.pose_it += 1
+
+    def goal_pose_cb(self, msg: PoseStamped):
+        msg.header.frame_id = "map"
+        self.move_spot_pub.publish(msg)
         self.arm_angle_proxy.call(self.base_arm_angles_request)
 
     def plan_viewpoint(self):
@@ -110,7 +196,7 @@ class SpotViewpointPlanner:
         go_to_pose.header.frame_id = "body"
         go_to_pose.pose = pose_msg
 
-        arm_angles = self.base_arm_angles
+        arm_angles = self.base_arm_angles.copy()
         arm_angles[0] = arm_angles[0] + random.uniform(-0.8, 0.8)
         arm_angles[4] = arm_angles[4] + random.uniform(-0.2, 0.2)
         arm_angles_req = ArmJointMovementRequest()
