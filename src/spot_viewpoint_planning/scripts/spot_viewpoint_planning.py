a05edcd5e7fe0383655a92a7a57cf44f3e7c01e0
Matthew Hanlon <hanlonm@ethz.ch>
2023-08-21 13:58:15 +0200

    experiment recording

--- src/spot_viewpoint_planning/scripts/spot_viewpoint_planning.py
+++ src/spot_viewpoint_planning/scripts/spot_viewpoint_planning.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+import os
 import rospy
 import numpy as np
 import random
 from pytransform3d import transformations as pt
 from pytransform3d import rotations as pr
+import cv2
+from cv_bridge import CvBridge
 
 
 from spot_msgs.srv import HandPose, HandPoseRequest, HandPoseResponse, ArmJointMovement, ArmJointMovementRequest, ArmJointMovementResponse
 from std_srvs.srv import Empty, EmptyRequest, EmptyResponse
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import Pose, PoseStamped, TransformStamped
 import tf2_ros
 
@@ -30,7 +34,7 @@ class SpotViewpointPlanner:
         self.base_arm_pose_request.duration = 0.0
         self.base_arm_pose_request.frame = "body"
         self.base_arm_pose = Pose()
-        self.base_arm_pose.position.x = 0.3
+        self.base_arm_pose.position.x = 0.29
         self.base_arm_pose.position.y = 0.0
         self.base_arm_pose.position.z = 0.7
         self.base_arm_pose.orientation.x = 0.0
@@ -133,8 +137,15 @@ class SpotViewpointPlanner:
 
         self.pose_it = 0
 
+        self.current_image_msg: Image = None
+        self.output_dir = "/workspace/src/output"
+        self.current_mode = None
+        self.current_exp_dir = None
+
+
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.bridge = CvBridge() 
 
         self.plan_viewpoint_pub = rospy.Publisher("plan_viewpoint", TransformStamped)
         self.plan_viewpoint_result_sub = rospy.Subscriber("plan_viewpoint_result_loc_cam", PoseStamped, self.viewpoint_result_cb)
@@ -142,6 +153,96 @@ class SpotViewpointPlanner:
         self.change_mode_pub = rospy.Publisher("planner_mode", String)
 
         self.goal_pose_sub = rospy.Subscriber("/goal_pose", PoseStamped, self.goal_pose_cb)
+
+        self.image_sub = rospy.Subscriber("/spot/camera/hand_color/image", Image, self.image_cb)
+        self.mode_sub = rospy.Subscriber("/current_planner_mode", String, self.current_mode_cb)
+
+        self.new_exp_service = rospy.Service("new_experiment", Empty, self.new_experiment_cb)
+        self.gt_loc_service = rospy.Service("gt_loc", Empty, self.gt_loc_cb)
+        self.save_current_service = rospy.Service("save_current", Empty, self.save_current_cb)
+
+
+
+    def image_cb(self, msg: Image):
+        self.current_image_msg = msg
+    
+    def current_mode_cb(self, msg: String):
+        self.current_mode = msg.data
+
+    def new_experiment_cb(self, req: EmptyRequest):
+        print("new experiment")
+        time = rospy.Time.now()
+        folder_name = str(time.secs)
+        self.current_exp_dir = self.output_dir + "/" + folder_name
+        os.mkdir(self.current_exp_dir)
+
+        return EmptyResponse()
+    
+    def gt_loc_cb(self, req: EmptyRequest):
+        try:
+            gt_folder = self.current_exp_dir + "gt"
+            os.mkdir(gt_folder)
+            current_image = self.bridge.imgmsg_to_cv2(
+                self.current_image_msg, desired_encoding="passthrough")
+            tf_dict = {}
+            tf_dict["tf_cam_tag"] = self.tf_buffer.lookup_transform(
+                target_frame="hand_color_image_sensor", source_frame="tag_2", time=rospy.Time(0), timeout=rospy.Duration(1.0))
+            tf_dict["tf_cam_body"] = self.tf_buffer.lookup_transform(
+                target_frame="hand_color_image_sensor", source_frame="body", time=rospy.Time(0), timeout=rospy.Duration(1.0))
+            tf_dict["tf_map_body"] = self.tf_buffer.lookup_transform(
+                target_frame="map", source_frame="body", time=rospy.Time(0), timeout=rospy.Duration(1.0))
+            pose_file = open(gt_folder + "/" "pose_file", "w")
+            pose_file.write("# tf_target_source tx ty tz qw qx qy qz")
+
+            for tf in tf_dict.keys():
+                transform: TransformStamped = tf_dict[tf]
+                pq = [transform.transform.translation.x,
+                      transform.transform.translation.y,
+                      transform.transform.translation.z,
+                      transform.transform.rotation.w,
+                      transform.transform.rotation.x,
+                      transform.transform.rotation.y,
+                      transform.transform.rotation.z]
+                pose_file.write("\n" + str(tf) + " " + " ".join(map(str, pq)))
+
+            cv2.imwrite(gt_folder + "/loc_image.jpg", current_image)
+
+        except Exception as e:
+            print(e)
+        return EmptyResponse()
+
+    def save_current_cb(self, req: EmptyRequest):
+        try:
+            method_folder = self.current_exp_dir + "/" + self.current_mode
+            os.mkdir(method_folder)
+            current_image = self.bridge.imgmsg_to_cv2(
+                self.current_image_msg, desired_encoding="passthrough")
+            tf_dict = {}
+            tf_dict["tf_map_bodyest"] = self.tf_buffer.lookup_transform(
+                target_frame="map", source_frame="body_est", time=rospy.Time(0), timeout=rospy.Duration(1.0))
+            tf_dict["tf_cam_body"] = self.tf_buffer.lookup_transform(
+                target_frame="hand_color_image_sensor", source_frame="body", time=rospy.Time(0), timeout=rospy.Duration(1.0))
+            tf_dict["tf_map_body"] = self.tf_buffer.lookup_transform(
+                target_frame="map", source_frame="body", time=rospy.Time(0), timeout=rospy.Duration(1.0))
+            pose_file = open(method_folder + "/" "pose_file", "w")
+            pose_file.write("# tf_target_source tx ty tz qw qx qy qz")
+
+            for tf in tf_dict.keys():
+                transform: TransformStamped = tf_dict[tf]
+                pq = [transform.transform.translation.x,
+                      transform.transform.translation.y,
+                      transform.transform.translation.z,
+                      transform.transform.rotation.w,
+                      transform.transform.rotation.x,
+                      transform.transform.rotation.y,
+                      transform.transform.rotation.z]
+                pose_file.write("\n" + str(tf) + " " + " ".join(map(str, pq)))
+
+            cv2.imwrite(method_folder + "/loc_image.jpg", current_image)
+
+        except Exception as e:
+            print(e)
+        return EmptyResponse()
 
     def viewpoint_result_cb(self, msg: PoseStamped):
         quat = np.array([msg.pose.orientation.w,msg.pose.orientation.x,msg.pose.orientation.y,msg.pose.orientation.z])
@@ -167,6 +268,7 @@ class SpotViewpointPlanner:
     def start_experiment_cb(self, req: EmptyRequest):
         self.exp_mode_it = 0
         self.arm_angle_proxy.call(self.base_arm_angles_request)
+        return EmptyResponse()
 
     def test_method_cb(self, req: EmptyRequest):
         self.arm_angle_proxy.call(self.base_arm_angles_request)
